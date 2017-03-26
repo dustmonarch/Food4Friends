d909be50ef094fced4d9ef7e75be7265ae77983e
dan <dsa8cx@virginia.edu>
2017-03-25 21:06:33 -0400

    commit 7

--- app/src/main/java/com/example/thomasrochman/food4friends/MainActivity.java
+++ app/src/main/java/com/example/thomasrochman/food4friends/MainActivity.java
@@ -5,9 +5,11 @@ import android.os.Bundle;
 import com.amazonaws.auth.CognitoCachingCredentialsProvider;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.dynamodbv2.*;
-import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.*
+//import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.*
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    //AmazonDynamoDBClient ddbClient = new AmazonDynamoDBClient(credentialsProvider);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
