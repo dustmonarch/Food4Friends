a7fd3fc6bc1a66f6462fd455303c20dc9fb5afce
dan <dsa8cx@virginia.edu>
2017-03-25 20:52:04 -0400

    Commit 6

--- app/src/main/java/com/example/thomasrochman/food4friends/MainActivity.java
+++ app/src/main/java/com/example/thomasrochman/food4friends/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.thomasrochman.food4friends;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.amazonaws.auth.CognitoCachingCredentialsProvider;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.dynamodbv2.*;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.*
 
 public class MainActivity extends AppCompatActivity {
 
